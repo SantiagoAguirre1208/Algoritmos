78c1d3b301d1c9884b24c4607aadeb08a8c55499
SantiagoAguirre1208 <saguirr14067@universidadean.edu.co>
2025-08-20 15:28:37 -0500

    Add files via upload

---
+++ clase_20-08_2.py
@@ -0,0 +1,3 @@
+print ("Hola este es mi segundo codigo---------------------------------------------------------------------------------------------")
+nombre = input ("¿Cual es tu nombre?")#Input es el equivalente de leer en PSEINT
+print ("Bienvenido" , nombre )
---
+++ clase_20-08_3.py
@@ -0,0 +1,3 @@
+edad = int(input ("Hola, ¿cual es tu edad?"))
+num2 = 5
+print ("tu edad dentro de 5 años va a ser: " , (edad + num2))
